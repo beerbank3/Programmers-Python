372f36f4bf316e92e9317af246a24fd7f4349756
beerbank3 <66869386+beerbank3@users.noreply.github.com>
2023-05-04 10:36:50 +0900

    [unrated] Title: 옹알이 (2), Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,12 @@
+def solution(babbling):
+    answer = 0
+    word_list = ['aya','ye','woo','ma']
+    for word in word_list:
+        for i, babble in enumerate(babbling):
+            if babble.count(word+word) > 0:
+                continue
+            babbling[i] = babble.replace(word,' ')
+    for i in babbling:
+        if not i.replace(' ', ''):
+            answer += 1
+    return answer
