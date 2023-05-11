79e1cb0621fa3976c5442cf07e6cba161951b7dd
ikjun.Choi <66869386+beerbank3@users.noreply.github.com>
2023-05-11 16:33:07 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = 0
     num = bin(n)[2:].count(str(1))
-    for i in range(n+1,n*2):
+    for i in range(n+1,n*2+1):
         if num == bin(i)[2:].count(str(1)):
             answer = i
             break
