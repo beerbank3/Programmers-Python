bf108ba643b5139032e40b2f6b8e315b854763b6
ikjun.Choi <66869386+beerbank3@users.noreply.github.com>
2023-05-15 17:26:59 +0900

    [level 2] Title: 짝지어 제거하기, Time: 127.65 ms, Memory: 20.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,17 @@
+import re
+def solution(s):
+    answer = 1
+    stack = []
+    pattern = re.compile(r'(\w)\1')
+    s = pattern.sub('',s)
+    for i in range(len(s)):
+        if not stack:
+            stack.append(s[i])
+        else:
+            if s[i] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+    if stack:
+        answer = 0
+    return answer
