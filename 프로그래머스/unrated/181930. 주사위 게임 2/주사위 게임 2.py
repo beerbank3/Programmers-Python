511877ad8da4014feee400938e84dc56f6fd896d
Ikjun Choi <66869386+beerbank3@users.noreply.github.com>
2023-08-22 18:32:53 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,7 @@
+def solution(a, b, c):
+    if a == b == c:  # 모두 같은 숫자인 경우
+        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    elif a == b or a == c or b == c:  # 두 숫자가 같은 경우
+        return (a + b + c) * (a**2 + b**2 + c**2)
+    else:  # 모두 다른 숫자인 경우
+        return a + b + c
